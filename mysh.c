823f5f48e74d573b3698474aad559bdc38625ae9
Hamad Naveed <56270097+HNaveed13@users.noreply.github.com>
2023-03-23 17:59:08 -0400

    Update mysh.c

--- mysh.c
+++ mysh.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
-#include <sys/wait.h>
+#include <fcntl.h>
+#include <errno.h>
+
 
 
 void execute_command(char** args) {
@@ -11,16 +12,21 @@ void execute_command(char** args) {
 }
 
 void batch_mode(FILE* fp) {
-    char command[1024];
-    while (fgets(command, 1024, fp) != NULL) {
-        // Remove newline character from the end of the command
-        command[strcspn(command, "\n")] = '\0';
+    char line[1024];
+    ssize_t num_read;
+
+    while ((num_read = read(fileno(fp), line, sizeof(line)-1)) > 0) {
+        // Null terminate the string and remove newline character
+        line[num_read] = '\0';
+        if (line[num_read-1] == '\n') {
+            line[num_read-1] = '\0';
+        }
 
         // Split the command into arguments
-        char* args[2];
-        char* token = strtok(command, " ");
+        char* args[1024];
+        char* token = strtok(line, " ");
         int num_args = 0;
-        while (token != NULL && num_args < 2) {
+        while (token != NULL && num_args < 1023) {
             args[num_args++] = token;
             token = strtok(NULL, " ");
         }
@@ -43,7 +49,15 @@ void interactive_mode() {
 
         // Read input from user
         char command[1024];
-        fgets(command, 1024, stdin);
+        ssize_t num_read = read(STDIN_FILENO, command, 1024);
+        if (num_read == -1) {
+            perror("Error reading input");
+            break;
+        } else if (num_read == 0) {
+            break;
+        } else {
+            command[num_read] = '\0';
+        }
 
         // Remove newline character from the end of the command
         command[strcspn(command, "\n")] = '\0';
@@ -72,7 +86,12 @@ void interactive_mode() {
 int main(int argc, char** argv) {
     if (argc > 1) {
         // Batch mode
-        FILE* fp = fopen(argv[1], "r");
+        int fd = open(argv[1], O_RDONLY);
+        if (fd == -1) {
+            perror("Error opening file");
+            exit(1);
+        }
+        FILE* fp = fdopen(fd, "r");
         if (fp == NULL) {
             perror("Error opening file");
             exit(1);
