938128b0e7172797cbe537c3e5c465a00e5865b3
chin2p <patelchirantan903@gmail.com>
2023-03-23 22:01:24 -0400

    newfile text.txt and some edge cases test.c

--- tests.c
+++ tests.c
@@ -1,1 +1,15 @@
+/*
+//edge testcases
+//program1 < input | program2 > output
+--fine
 
+program1 < input > output1 | program2
+--error, dont allow and report error
+
+program1 | program2 < input
+--error, dont allow and error
+
+
+
+
+*/
