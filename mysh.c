7d4c5b2850ea96336391f958997294b347754d50
Hamad Naveed <56270097+HNaveed13@users.noreply.github.com>
2023-03-23 20:59:10 -0400

    Update mysh.c

--- mysh.c
+++ mysh.c
@@ -3,41 +3,51 @@
 #include <string.h>
 #include <fcntl.h>
 #include <errno.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <fnmatch.h>
+
+void process_line(char* line);
+const char *search_paths[] = {
+        "/usr/local/sbin/",
+        "/usr/local/bin/",
+        "/usr/sbin/",
+        "/usr/bin/",
+        "/sbin/",
+        "/bin/"
+};
 
 
 
-void execute_command(char** args) {
-    // commands here
-    printf("Command executed: %s\n", args[0]);
-}
-
-void batch_mode(FILE* fp) {
+void batch_mode(FILE *fp) {
     char line[1024];
-    ssize_t num_read;
+    int index = 0;
+    int c;
 
-    while ((num_read = read(fileno(fp), line, sizeof(line)-1)) > 0) {
-        // Null terminate the string and remove newline character
-        line[num_read] = '\0';
-        if (line[num_read-1] == '\n') {
-            line[num_read-1] = '\0';
+    while ((c = getc(fp)) != EOF) {
+        if (c == '\n') {
+            line[index] = '\0';
+
+            if (strcmp(line, "exit") == 0) {
+                break;
+            }
+            process_line(line);
+            index = 0;
+        } else {
+            line[index++] = c;
+            if (index >= sizeof(line)) {
+                fprintf(stderr, "Error: input line too long\n");
+                break;
+            }
         }
-
-        // Split the command into arguments
-        char* args[1024];
-        char* token = strtok(line, " ");
-        int num_args = 0;
-        while (token != NULL && num_args < 1023) {
-            args[num_args++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[num_args] = NULL;
-
-        // Execute the command
-        execute_command(args);
-
-        // Exit if the command is "exit"
-        if (strcmp(args[0], "exit") == 0) {
-            break;
+    }
+    if (index > 0) {
+        line[index] = '\0';
+        if (strcmp(line, "exit") != 0) {
+            process_line(line);
         }
     }
 }
@@ -47,60 +57,230 @@ void interactive_mode() {
     while (1) {
         printf("mysh> ");
 
-        // Read input from user
         char command[1024];
-        ssize_t num_read = read(STDIN_FILENO, command, 1024);
-        if (num_read == -1) {
-            perror("Error reading input");
-            break;
-        } else if (num_read == 0) {
-            break;
-        } else {
-            command[num_read] = '\0';
+        int command_length = 0;
+        char ch;
+        ssize_t read_result;
+
+        while ((read_result = read(STDIN_FILENO, &ch, 1)) > 0) {
+            if (ch == '\n') {
+                break;
+            }
+            command[command_length++] = ch;
         }
 
-        // Remove newline character from the end of the command
-        command[strcspn(command, "\n")] = '\0';
+        if (read_result == -1) {
+            perror("Error reading from stdin");
+            break;
+        }
 
-        // Exit if the command is "exit"
+        if (read_result == 0) { // EOF
+            break;
+        }
+
+        command[command_length] = '\0';
+
         if (strcmp(command, "exit") == 0) {
             break;
         }
 
-        // Split the command into arguments
-        char* args[2];
-        char* token = strtok(command, " ");
-        int num_args = 0;
-        while (token != NULL && num_args < 2) {
-            args[num_args++] = token;
-            token = strtok(NULL, " ");
-        }
-        args[num_args] = NULL;
-
-        // Execute the command
-        execute_command(args);
+        process_line(command);
     }
     printf("Goodbye!\n");
 }
 
 int main(int argc, char** argv) {
     if (argc > 1) {
-        // Batch mode
         int fd = open(argv[1], O_RDONLY);
         if (fd == -1) {
             perror("Error opening file");
-            exit(1);
+            return 1;
         }
-        FILE* fp = fdopen(fd, "r");
-        if (fp == NULL) {
-            perror("Error opening file");
-            exit(1);
-        }
-        batch_mode(fp);
-        fclose(fp);
+        // redirect standard input to the opened file descriptor
+        dup2(fd, STDIN_FILENO);
+
+        batch_mode(stdin);
+
+        close(fd);      // close the opened file descriptor
     } else {
-        // Interactive mode
         interactive_mode();
     }
+
     return 0;
+}
+
+char *find_command_path(const char *command) {
+    struct stat sb;
+    char *path = NULL;
+
+    for (int i = 0; i < sizeof(search_paths) / sizeof(search_paths[0]); ++i) {
+        path = malloc(strlen(search_paths[i]) + strlen(command) + 1);
+        strcpy(path, search_paths[i]);
+        strcat(path, command);
+
+        if (stat(path, &sb) == 0 && sb.st_mode & S_IXUSR) {
+            return path;
+        }
+
+        free(path);
+    }
+
+    return NULL;
+}
+
+void execute_command(char** args, int in_fd, int out_fd) {
+    int i = 0;
+    while (args[i] != NULL) {
+        if (strcmp(args[i], "<") == 0) {
+            args[i] = NULL;
+            in_fd = open(args[i + 1], O_RDONLY);
+            if (in_fd < 0) {
+                perror("Error opening input file");
+                exit(1);
+            }
+            i++;
+        } else if (strcmp(args[i], ">") == 0) {
+            args[i] = NULL;
+            out_fd = open(args[i + 1], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP);
+            if (out_fd < 0) {
+                perror("Error opening output file");
+                exit(1);
+            }
+            i++;
+        }
+        i++;
+    }
+    pid_t pid = fork();
+    if (pid == 0) {
+        if (in_fd != STDIN_FILENO) {
+            dup2(in_fd, STDIN_FILENO);
+            close(in_fd);
+        }
+        if (out_fd != STDOUT_FILENO) {
+            dup2(out_fd, STDOUT_FILENO);
+            close(out_fd);
+        }
+        char *command_path = find_command_path(args[0]);
+        if (command_path != NULL) {
+            execv(command_path, args);
+            free(command_path);
+        } else {
+            execvp(args[0], args);
+        }
+
+        if (strchr(args[0], '/') != NULL) {
+            execv(args[0], args);
+        } else {
+            execvp(args[0], args);
+        }
+        perror("Error executing command");
+        exit(1);
+    } else if (pid < 0) {
+        perror("Error forking");
+        exit(1);
+    } else {
+        close(in_fd);
+        close(out_fd);
+        waitpid(pid, NULL, 0);
+    }
+}
+
+void handle_wildcard(char* pattern, char** args, int* num_args) {
+    char* dir_path = ".";
+    char* last_slash = strrchr(pattern, '/');
+    if (last_slash != NULL) {
+        *last_slash = '\0';
+        dir_path = pattern;
+        pattern = last_slash + 1;
+    }
+
+    DIR* dir = opendir(dir_path);
+    if (dir == NULL) {
+        perror("Error opening directory");
+        return;
+    }
+
+    struct dirent* entry;
+
+    while ((entry = readdir(dir)) != NULL) {
+        if (fnmatch(pattern, entry->d_name, 0) == 0 && !(pattern[0] == '*' && entry->d_name[0] == '.')) {
+            char* matched_path;
+            if (last_slash != NULL) {
+                int len = snprintf(NULL, 0, "%s/%s", dir_path, entry->d_name);
+                char* matched_path = malloc(len + 1);
+                if (matched_path == NULL) {
+                    perror("Memory allocation error");
+                    return;
+                }
+                sprintf(matched_path, "%s/%s", dir_path, entry->d_name);
+            } else {
+                matched_path = strdup(entry->d_name);
+            }
+            args[*num_args] = matched_path;
+            (*num_args)++;
+        }
+    }
+
+    closedir(dir);
+}
+
+void process_line(char* line) {
+    char* args[1024];
+    int num_args = 0;
+    int in_fd = STDIN_FILENO;
+    int out_fd = STDOUT_FILENO;
+    int pipe_fds[2] = {-1, -1};
+
+    char* token = strtok(line, " ");
+    while (token != NULL) {
+        if (strcmp(token, "|") == 0) {
+            if (pipe(pipe_fds) == -1) {
+                perror("Error creating pipe");
+                return;
+            }
+            out_fd = pipe_fds[1];
+        } else if (strcmp(token, "<") == 0) {
+            token = strtok(NULL, " ");
+            if (token == NULL) {
+                fprintf(stderr, "Error: missing file after <\n");
+                return;
+            }
+            in_fd = open(token, O_RDONLY);
+            if (in_fd == -1) {
+                perror("Error opening file for input");
+                return;
+            }
+        } else if (strcmp(token, ">") == 0) {
+            token = strtok(NULL, " ");
+            if (token == NULL) {
+                fprintf(stderr, "Error: missing file after >\n");
+                return;
+            }
+            out_fd = open(token, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (out_fd == -1) {
+                perror("Error opening file for output");
+                return;
+            }
+        } else if (strchr(token, '*') != NULL) {
+            handle_wildcard(token, args, &num_args);
+        } else {
+            args[num_args++] = token;
+        }
+
+        if (out_fd != STDOUT_FILENO) {
+            args[num_args] = NULL;
+            execute_command(args, in_fd, out_fd);
+            num_args = 0;
+            in_fd = pipe_fds[0];
+            out_fd = STDOUT_FILENO;
+            close(pipe_fds[1]);
+        }
+
+        token = strtok(NULL, " ");
+    }
+
+    if (num_args > 0) {
+        args[num_args] = NULL;
+        execute_command(args, in_fd, out_fd);
+    }
 }
