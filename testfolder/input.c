d2af30293c2b542393daf942112fb8532061237d
chin2p <patelchirantan903@gmail.com>
2023-03-26 18:02:22 -0400

    update input.c

--- testfolder/input.c
+++ testfolder/input.c
@@ -1,29 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-#define MAX_INPUT_LENGTH 256
-
-int main() {
-  char input[MAX_INPUT_LENGTH];
-  int n;
-
-  while (1) {
-    write(STDOUT_FILENO, "Input something here: ", 22);
-    n = read(STDIN_FILENO, input, MAX_INPUT_LENGTH);
-
-    // remove newline character from input
-    input[strcspn(input, "\n")] = '\0';
-
-    if (n == 0 || strncmp(input, "exit", 4) == 0) {
-      write(STDOUT_FILENO, "\n", 1);
-      break;
+int main(int argc, char *argv[]) {
+    char *path = getenv("PATH");   // get the PATH environment variable
+    char *dir;                     // pointer to each directory in the PATH
+    char *cmd;                     // pointer to the command we're searching for
+    char prog[256];                // buffer to hold the full path to the command
+    int found = 0;                 // flag to indicate if we've found the command
+    
+    // check if the PATH environment variable exists
+    if (path == NULL) {
+        fprintf(stderr, "Error: PATH environment variable is not set.\n");
+        exit(1);
     }
-
-    write(STDOUT_FILENO, "You entered: ", 13);
-    write(STDOUT_FILENO, input, strlen(input));
-    write(STDOUT_FILENO, "\n", 1);
-  }
-
-  return 0;
+    
+    // copy the command we're searching for (i.e. "ls") into a buffer
+    cmd = argv[1];
+    
+    // loop through each directory in the PATH
+    while ((dir = strsep(&path, ":")) != NULL) {
+        // build the full path to the command by concatenating the directory and command names
+        sprintf(prog, "%s/%s", dir, cmd);
+        
+        // check if the file exists and is executable
+        if (access(prog, X_OK) == 0) {
+            // found the command, set the flag and break out of the loop
+            found = 1;
+            break;
+        }
+    }
+    
+    if (!found) {
+        fprintf(stderr, "Error: %s command not found.\n", cmd);
+        exit(1);
+    }
+    
+    // execute the command with any arguments that were passed in
+    execv(prog, argv + 1);
+    
+    // if execv returns, there was an error
+    fprintf(stderr, "Error: failed to execute %s.\n", cmd);
+    exit(1);
 }
--- tests.c
+++ tests.c
@@ -1,15 +1,5 @@
-/*
-//edge testcases
-//program1 < input | program2 > output
---fine
-
-program1 < input > output1 | program2
---error, dont allow and report error
-
-program1 | program2 < input
---error, dont allow and error
-
-
-
-
-*/
+int main(int argc, char const *argv[])
+{
+    printf("Input: ", argv[1]);
+    return 0;
+}
